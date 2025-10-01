c029c3a7f59e26f3b770c94a2e82bcf3e752e250
meama24252525 <m.franchise@meama.ge>
2025-10-01 14:45:59 +0400

    Update app.js

--- app.js
+++ app.js
@@ -200,25 +200,6 @@ class MenuManager {
     }
 }
 
-class VideoPlayer {
-    constructor() {
-        this.video = document.getElementById('fullscreenVideo');
-        this.container = document.getElementById('fullscreen');
-    }
-
-    play(url) {
-        this.video.src = url;
-        this.container.classList.add('show');
-        this.video.play();
-    }
-
-    close() {
-        this.container.classList.remove('show');
-        this.video.pause();
-        this.video.src = '';
-    }
-}
-
 // Initialize the app
 const menuManager = new MenuManager();
 window.menuManager = menuManager;
